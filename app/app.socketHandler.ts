f74dc93bfc151d92c0c64545c96704d22bc4f230
n02220249 <pasqualeguy@gmail.com>
2016-08-07 16:06:33 -0400

    spacing changes

--- app/app.socketHandler.ts
+++ app/app.socketHandler.ts
@@ -1,12 +1,12 @@
 export class AppSocketHandler {
-	msgs: string[];
-	ws: WebSocket;
+  msgs: string[];
+  ws: WebSocket;
 
   constructor(){
     this.msgs = [];
     this.ws = new WebSocket("ws://localhost:5000/echo");
     this.ws.onmessage = function (evt)                  //create listener for incoming messages
-      { 
+      {
         var received_msg = evt.data;
         this.msgs = String(received_msg).split(',');    //convert message into array and set as this.msgs
       }.bind(this);                                     // bind this context to have scope of this.msgs
